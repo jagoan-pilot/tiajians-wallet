8f01557f584f9a3ab6fb376ab4e9f8caa40fe4af
Tomasz Ludek <tomasz@dash.org>
2020-06-05 01:29:57 +0200

    Test message

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -17,10 +17,13 @@ if [ "${TRAVIS_TAG:0:4^^}" = "NMA-" ] || [ "${TRAVIS_TAG:0:4}" = "dpl-" ]; then
   cp wallet/build/outputs/apk/_testNet3/debug/dash-wallet-_testNet3-debug.apk "$DEPLOY_DIR"/dash-wallet-_testNet3-debug.apk
   cp wallet/build/outputs/apk/prod/debug/dash-wallet-prod-debug.apk "$DEPLOY_DIR"/dash-wallet-prod-debug.apk
 #  cp wallet/build/outputs/apk/_testNet3/debug/dash-wallet-_testNet3-debug.apk dash-wallet-staging/"$TRAVIS_TAG"/dash-wallet-_testNet3-debug.apk
+  README="$DEPLOY_DIR"/README.md
   if [ "${TRAVIS_TAG:0:4^^}" = "NMA-" ]; then
-    printf 'https://dashpay.atlassian.net/browse/%s\n\n' "$TRAVIS_TAG" > "$DEPLOY_DIR"/README.md
+    printf 'https://dashpay.atlassian.net/browse/%s\n\n' "$TRAVIS_TAG" > "$README"
   fi
-  git show "$TRAVIS_TAG" >> "$DEPLOY_DIR"/README.md
+  git show "$TRAVIS_TAG" | grep -B20 "diff" >> "$README"
+  head -n -1 "$README" > temp.txt ; mv temp.txt "$README"
+
   cd dash-wallet-staging || exit
   git add .
   git commit -m "Travis CI deploy $TRAVIS_TAG"
